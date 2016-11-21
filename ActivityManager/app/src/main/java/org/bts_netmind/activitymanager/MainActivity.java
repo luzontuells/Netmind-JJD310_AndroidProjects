80eaf6f594af2180c22fc718a56c12ee3a65714c
Raul Garcia <raul.garcia.santiago93@gmail.com>
2016-11-21 10:10:31 +0100

    test-Quimi

--- ActivityManager/app/src/main/java/org/bts_netmind/activitymanager/MainActivity.java
+++ ActivityManager/app/src/main/java/org/bts_netmind/activitymanager/MainActivity.java
@@ -2,8 +2,8 @@ package org.bts_netmind.activitymanager;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
+import android.os.Bundle; 
+import android.util.Log;  
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
